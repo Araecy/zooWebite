11a736a6d02974f46140304e1784f2fcddb915f7
Araecy <74868163+Araecy@users.noreply.github.com>
2021-10-28 14:54:13 +0200

    Update header.css/php

--- header.php
+++ header.php
@@ -1,15 +1,17 @@
-<nav class="dropdownmenu" class="head">
-  <ul>
-    <li><a href="index.php">Home</a></li>
-    <li><a href="about.php">About</a></li>
-    <li><a href="#">Articles archives</a>
-      <ul id="submenu">
-        <li><a href="#">Our zoo</a></li>
-        <li><a href="#">Zoo monthly spendings</a></li>
-        <li><a href="#">Creating new space for our animals</a></li>
+<div class="head">
+    <nav class="dropdownmenu">
+      <ul>
+        <li><a href="index.php">Home</a></li>
+        <li><a href="about.php">About</a></li>
+        <li><a href="#">Articles archives</a>
+          <ul id="submenu">
+            <li><a href="#">Our zoo</a></li>
+            <li><a href="#">Zoo monthly spendings</a></li>
+            <li><a href="#">Creating new space for our animals</a></li>
+          </ul>
+        </li>
+        <li><a href="#">News</a></li>
+        <li><a href="#">Contact Us</a></li>
       </ul>
-    </li>
-    <li><a href="#">News</a></li>
-    <li><a href="#">Contact Us</a></li>
-  </ul>
-</nav>
+    </nav>
+</div>
