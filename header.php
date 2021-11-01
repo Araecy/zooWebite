d0eaaa5054cdf0d11146f19bfe3809c1a74e8d0c
Araecy <Araecy.w@gmail.com>
2021-11-01 11:43:22 +0100

    Added

--- header.php
+++ header.php
@@ -1,17 +1,9 @@
-<div class="head">
-    <nav class="dropdownmenu">
-      <ul>
-        <li><a href="index.php">Home</a></li>
-        <li><a href="about.php">About</a></li>
-        <li><a href="#">Articles archives</a>
-          <ul id="submenu">
-            <li><a href="#">Our zoo</a></li>
-            <li><a href="#">Zoo monthly spendings</a></li>
-            <li><a href="#">Creating new space for our animals</a></li>
-          </ul>
-        </li>
-        <li><a href="#">News</a></li>
-        <li><a href="#">Contact Us</a></li>
-      </ul>
-    </nav>
-</div>
+<nav id="mainMenu">
+    <ul class="navBar">
+      <li class="navBtnHome"><a href="index.php">Home</a></li>
+      <li class="navBtnAbout"><a href="about.php">About</a></li>
+      <li class="navBtnServices"><a href="services.php">Services</a></li>
+      <li class="navBtnProducts"><a href="products.php">Products</a></li>
+      <li class="navBtnContacts"><a href="contacts.php">Contacts</a></li>
+    </ul>
+</nav>
