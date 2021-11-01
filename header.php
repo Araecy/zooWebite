0ce2c0c4fdeaf91310c68f59f0524111a2e92bd2
Araecy <Araecy.w@gmail.com>
2021-11-01 12:21:14 +0100

    added

--- header.php
+++ header.php
@@ -1,9 +1,19 @@
 <nav id="mainMenu">
-    <ul class="navBar">
-      <li class="navBtnHome"><a href="index.php">Home</a></li>
-      <li class="navBtnAbout"><a href="about.php">About</a></li>
-      <li class="navBtnServices"><a href="services.php">Services</a></li>
-      <li class="navBtnProducts"><a href="products.php">Products</a></li>
-      <li class="navBtnContacts"><a href="contacts.php">Contacts</a></li>
-    </ul>
+  <ul class="navBar">
+    <div class="dropdown">
+      <button class="dropBtn">Dropdown</button>
+      <div class="dropdownContent">
+        <a href="#">Link 1</a>
+        <a href="#">Link 2</a>
+        <a href="#">Link 3</a>
+      </div>
+    </div>
+    <li class="navBtnHome"><a href="index.php">Home</a></li>
+    <li class="navBtnAbout"><a href="about.php">About the zoo</a></li>
+    <li class="navBtnServices"><a href="services.php">Services</a></li>
+    <li class="navBtnProducts"><a href="products.php">Products</a></li>
+    <li class="navBtnContacts"><a href="contacts.php">Contacts</a></li>
+  </ul>
 </nav>
+
+<!-- https://www.w3schools.com/howto/howto_css_dropdown.asp dropdown menu stuff  -->
