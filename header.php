e276241afb39757eb5f35d4e2839088f9535ef4d
Araecy <Araecy.w@gmail.com>
2021-11-08 12:26:43 +0100

    Newly added

--- header.php
+++ header.php
@@ -1,23 +1,68 @@
-<nav id="mainMenu">
-  <ul class="navBar">
-    <div class="dropdown">
-      <!-- NOTE 
+<nav class="mainNavContainer">
+  <ul>
+    <li><a href="index.php">Home page</a>
+    </li>
+
+    <li><a href="index.php">Tickets</a>
+      <span class="arrow">&#9660;</span>
+      <ul class="dropdownContents">
+        <li><a href="numbersDatesAndFacts.php">Numbers, dates and facts</a></li>
+        <li><a href="history.php">History</a></li>
+        <li><a href="missions.php">Missions</a></li>
+        <li><a href="partnerships.php">Partnerships</a></li>
+        <li><a href="careers.php">Careers</a></li>
+      </ul>
+    </li>
+    
+    <li>About us
+      <span class="arrow">&#9660;</span>
+      <ul class="dropdownContents">
+        <li>Cpu</li>
+        <li>GPU</li>
+        <li>PSU</li>
+      </ul>
+    </li>
+    <li>Drink
+      <span class="arrow">&#9660;</span>
+      <ul class="dropdownContents">
+        <li>Beer</li>
+        <li>Cola</li>
+        <li>Safari</li>
+      </ul>
+    </li>
+  </ul>
+
+</nav>
+
+
+
+
+
+
+
+
+
+
+<!-- <nav id="mainMenu"> -->
+<!-- <ul class="navBar">
+    <div class="dropdown"> -->
+<!-- NOTE 
           BEKIJK HTML/CSS 2 DROPDOWN MENU zal 100% beter worden.
     -->
-      <div class="dropBtn navContainer" onclick="location.href='index.php';">Home</div>
+<!-- <div class="dropBtn navContainer" onclick="location.href='index.php';">Home</div>
       <div class="dropdownContent">
         <li><a href="#">link 1</a></li>
         <li><a href="#">link 2</a></li>
         <li><a href="#">link 3</a></li>
       </div>
-    </div>
-    <!-- <div class="dropBtn navContainer" onclick="location.href='index.php';">help</div> -->
-    <!-- <li class="navBtnHome navContainer"><a href="index.php">Home</a></li> -->
-    <li class="navBtnAbout navContainer"><a href="about.php">About the zoo</a></li>
+    </div> -->
+<!-- <div class="dropBtn navContainer" onclick="location.href='index.php';">help</div> -->
+<!-- <li class="navBtnHome navContainer"><a href="index.php">Home</a></li> -->
+<!-- <li class="navBtnAbout navContainer"><a href="about.php">About the zoo</a></li>
     <li class="navBtnServices navContainer"><a href="services.php">Services</a></li>
     <li class="navBtnProducts navContainer"><a href="products.php">Products</a></li>
     <li class="navBtnContacts navContainer"><a href="contacts.php">Contacts</a></li>
-  </ul>
-</nav>
+  </ul> -->
+<!-- </nav> -->
 
 <!-- https://www.w3schools.com/howto/howto_css_dropdown.asp dropdown menu stuff  -->
